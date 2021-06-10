34776d0b4c42a73a2a95aa94afe11059e6267ceb
Amod SY <amod.isozymes@gmail.com>
2021-06-10 23:58:09 +0530

    final

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import Lists from "./Lists";
 
 const Notifiations = () => {
@@ -8,6 +8,7 @@ const Notifiations = () => {
     const[showActive, setActive] = useState(false);
     const[searchTxt, setSearchTxt] = useState("");
     
+    //to handle API call on page load
     useEffect(() => {
         fetch("https://c466ca5e-0412-4dee-a7cb-492868a09974.mock.pstmn.io/notifications/")
             .then((response)=>response.json())
@@ -17,6 +18,7 @@ const Notifiations = () => {
             })
     }, [])
 
+    //to handle checkbox toggle
     const toggleActive = () => {
         if(!showActive){
             setActive(!showActive);
@@ -28,22 +30,42 @@ const Notifiations = () => {
         }
     }
 
+    //debounce cll back fn for search
+    const debounce = (func) => {
+        let timer;
+        return function(...args){
+            const context = this;
+            if (timer) clearTimeout(timer)
+            timer = setTimeout(() => {
+                timer = null;
+                func.apply(context, args)
+            }, 500)
+        }
+    }
+
+    //fn to han dle search on key change
     const handleSearch = (event) => {
-        setSearchTxt(event.target.value);
-        setTimeout(()=> {
-            searchNotifications();
-        },100)
+        let val = event.target.value;
+        handleSearchFilter(val)
     }
-    const searchNotifications = () => {
-        let filterData = originalData.filter((item) => item.information.includes(searchTxt))
-        setNotfnList(filterData);
+
+    //search filter method
+    const handleSearchFilter = (val) => {        
+        let filterData = [...originalData].filter((item) => item.information.includes(val));
+        if(filterData.length > 0){
+            setNotfnList(filterData);
+        } 
     }
+
+    //memoizing the debounce mehtod
+    const optimised = useCallback(debounce(handleSearch), []);
+
     return(        
       <div className="container">
         <h1>Notifications</h1>
         <div className="container-elm">
             <span>Search: 
-                <input type="text" name="searchTxt" value={searchTxt} onChange={(e) => handleSearch(e)}/>
+                <input type="text" name="searchTxt" onChange={handleSearch}/>
             </span>
             <span className="item-right"> 
                 <input 
